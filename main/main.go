3f7dd636db3df36ada4ec56f679da5657b679efa
Alex Pavlov <apavlov@Alexs-MacBook-Pro.local>
2024-02-13 20:16:26 -0500

    (closes #5) imiplement dynamic tile generation

---
+++ main/main.go
@@ -0,0 +1,34 @@
+components {
+  id: "main"
+  component: "/main/main.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tile_factory"
+  type: "factory"
+  data: "prototype: \"/assets/resources/tile.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
