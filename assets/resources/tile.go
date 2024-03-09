703a02e16d7fc122947a3ac5e2b9f6a615860fbb
Alex <alexpavlov0525@gmail.com>
2024-03-08 21:19:49 -0500

    address feedback

--- assets/resources/tile.go
+++ assets/resources/tile.go
@@ -13,21 +13,6 @@ components {
     w: 1.0
   }
 }
-components {
-  id: "tile_hit"
-  component: "/assets/resources/tile_hit.particlefx"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
