0333613d38c18567fc531614ea2b38271fe9fe1a
pixelpusherninja <dauksts.oskars@gmail.com>
2024-05-25 01:20:37 -0700

    Add dynamic tree generation - first pass

---
+++ assets/background/tree.go
@@ -0,0 +1,38 @@
+components {
+  id: "tree"
+  component: "/assets/background/tree.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"oak_tree_animation\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/background/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
