53d550ad7b7f1eb506c134ddd9e309282e63f09b
pixelpusherninja <dauksts.oskars@gmail.com>
2024-06-14 09:21:01 -0700

    Background generation improvements
    - Add 3 new background sprites (tree2, bush1, bush2)
    - Randomize speed of each sprites animation
    - Randomize starting frame of each sprites animation

--- assets/background/tree.go
+++ assets/background/tree.go
@@ -14,9 +14,78 @@ components {
   }
 }
 embedded_components {
-  id: "sprite"
+  id: "tree_one"
   type: "sprite"
-  data: "default_animation: \"oak_tree_animation\"\n"
+  data: "default_animation: \"tree_one\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/background/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "tree_two"
+  type: "sprite"
+  data: "default_animation: \"tree_two\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/background/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bush_one"
+  type: "sprite"
+  data: "default_animation: \"bush_one\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/background/background.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bush_two"
+  type: "sprite"
+  data: "default_animation: \"bush_two\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   "textures {\n"
